253acc83a6df0181676406a89aea09085f386aa7
afarevalo <af.arevalo@uniandes.edu.co>
2024-05-26 17:21:15 -0500

    Cambio varios en el codigo del Mapa
    Nuevas salidas

--- Taller No.3/2. Código/Mapa y estaditicas.R
+++ Taller No.3/2. Código/Mapa y estaditicas.R
@@ -1,28 +1,21 @@
-# Instalar y cargar las bibliotecas necesarias
-#install.packages("sf")
-#install.packages("ggplot2")
-#install.packages("readxl")
-#install.packages("dplyr")
-
-if (!require("pacman")) install.packages("pacman") # Isntalar pacman (sino está instalada)
-require(pacman) # llamar pacman
 
 # Limpiar el entorno
 rm(list = ls())
 
-## llamar y/o instalar librerias
-p_load(tidyverse,rio,
-       viridis, ## paletas de colores 
-       sf, ## datos espaciales
-       leaflet, ## visualizaciones
-       tmaptools, ## geocodificar
-       ggsn, ## map scale bar 
-       tidycensus) ## packages with census data
-
+#Librerias
+library(pacman)
+library(tidyverse)
+library(rio)
+library(viridis)
+library(leaflet)
+library(tmaptools)
+library(tidycensus)
 library(dplyr)
 library(sf)
 library(ggplot2)
 library(readxl)
+library(summarytools)
+library(officer)
 
 # Cargar los datos desde un archivo Excel
 bog <- read_excel("1. Datos/Consolidado.xlsx", 
@@ -40,6 +33,30 @@ bog <- bog %>%
     lon = as.numeric(lon),
     lat = as.numeric(lat)
   )
+
+# Seleccionar las variables de interés
+variables <- c("price", "rooms", "property_type", "Comercial", "Inflacion_Acum", "Precio_04_2024")
+bog_subset <- bog[, variables]
+
+# Obtener estadísticas descriptivas
+desc_stats <- summarytools::descr(bog_subset)
+
+# Crear un nuevo documento de Word
+doc <- read_docx()
+
+# Agregar el título
+doc <- doc %>%
+  body_add_par("Estadísticas Descriptivas", style = "heading 1")
+
+# Convertir las estadísticas descriptivas a una tabla
+flextable_desc_stats <- flextable::flextable(desc_stats)
+
+# Agregar las estadísticas descriptivas al documento como tabla
+doc <- doc %>%
+  body_add_flextable(flextable_desc_stats)
+
+# Guardar el documento de Word en la ruta especificada
+print(doc, "C:/Users/windows/Documents/GitHub/Problem_Set_1/Taller-No.3/Taller No.3/3. Tablas/estadisticas_descriptivas.docx") 
 
 # Verificar si 'price' está presente y tiene valores correctos
 summary(bog$price)
@@ -61,7 +78,7 @@ p <- ggplot(data = bog_sf) +
 # Mostrar el gráfico
 print(p)
 
-ggsave(plot=p , filename="C:/Users/windows/Documents/GitHub/Problem_Set_1/Taller-No.3/Taller No.3/4. Gráficos/mapa_Hogares.pdf" , width=6.5 , height=8)
+ggsave(plot=p , filename="C:/Users/windows/Documents/GitHub/Problem_Set_1/Taller-No.3/Taller No.3/4. Gráficos/mapa_Hogares.png" , width=6.5 , height=8)
 
 ## Otro MAPA de Bogota.
 
@@ -87,12 +104,9 @@ p = ggplot(data=bog) + geom_sf(mapping = aes(fill=q_price) , size=0.3 , col=NA) 
 p
 
 ## save plot
-ggsave(plot=p , filename="C:/Users/windows/Documents/GitHub/Problem_Set_1/Taller-No.3/Taller No.3/4. Gráficos/mapa_Valor_Medio.pdf" , width=6.5 , height=8)
+ggsave(plot=p , filename="C:/Users/windows/Documents/GitHub/Problem_Set_1/Taller-No.3/Taller No.3/4. Gráficos/mapa_Valor_Medio.png" , width=6.5 , height=8)
 
-
-
-##=== plot distances ===##
-
+  
 ## plot 
 ggplot(bog, aes(x=dist_CBD/1000, y=price_surface_median/1000000)) +
   geom_point(shape=1) + geom_smooth(method="lm") + 
@@ -100,5 +114,5 @@ ggplot(bog, aes(x=dist_CBD/1000, y=price_surface_median/1000000)) +
                     y="Valor mediano del metro cuadrado (millones)")
 
 ## save plot
-ggsave(filename="output/bog_dist_cbd.png" , width=6.5 , height=7)
+ggsave(filename="C:/Users/windows/Documents/GitHub/Problem_Set_1/Taller-No.3/Taller No.3/4. Gráficos/Distancia a negocios.png" , width=6.5 , height=7)
 
